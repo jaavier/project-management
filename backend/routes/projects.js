7c2530f5109d55b6e10fd5371f7f4729f180c711
Javier G <javier.guajardoj@gmail.com>
2022-01-19 12:28:23 +0100

    Route to create project in db

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,3 +1,4 @@
+const Project = require('../models/project');
 const { Router } = require('express');
 const router = Router();
 
@@ -5,6 +6,23 @@ router.get('/', async (req, res) => {
 	res.json({
 		message: 'Hello World from Projects'
 	});
+});
+
+router.post('/', async (req, res) => {
+	const { name, image, url, github, technologies, description, milestones } = req.body;
+	try {
+		const newProject = new Project(req.body);
+		await newProject.save();
+		res.json({
+			message: 'Project created successfully',
+			data: newProject
+		});
+	} catch (e) {
+		console.log('🚀 ~ file: projects.js ~ line 22 ~ router.post ~ e', e);
+		res.json({
+			message: 'Something went wrong'
+		});
+	}
 });
 
 module.exports = router;
