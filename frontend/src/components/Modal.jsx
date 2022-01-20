27b25cfe7e8505509c982b802667130ff73794e9
Javier G <javier.guajardoj@gmail.com>
2022-01-20 13:31:50 +0100

    Add button to close modal and convert div into button

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,11 +1,13 @@
 import { useModal } from "../context/modal"
-import Milestone from "./Milestone"
 
 export default function Modal(props) {
     const { isOpen, setIsOpen, component } = useModal()
     return isOpen ? (
         <div className="absolute top-0 bg-white  text-black w-full h-screen">
-            <div className="w-screen mx-auto container bg-gray-100 opacity-100 border p-5 rounded">
+            <div className="w-screen mx-auto container bg-gray-100 opacity-100 border p-5 rounded relative">
+                <div className="absolute right-5 top-5">
+                    <button onClick={() => setIsOpen(false)}>&times;</button>
+                </div>
                 {component}
             </div>
         </div>
